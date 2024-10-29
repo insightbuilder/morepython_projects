c6ad080f41bd347f1d429efb05dd696b48fd2855
insightbuilder <kamaljp@gmail.com>
2024-10-29 07:34:04 +0530

    started sysargv exercise

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ https://github.com/zedshaw/learn-more-python-the-hard-way-solutions
 
 The videos explaining the solutions are present at
 base_url="https://ptgmedia.pearsoncmg.com/imprint_downloads/informit/bookreg/9780134123486"
+
+Following are the Journal updates on the Chapters
---
+++ exercise4/sysargv.py
@@ -0,0 +1,73 @@
+from sys import argv
+
+# since help will be printed many times, creating function
+
+
+def print_help(msg=" "):
+    print(
+        f"""
+Issue is: {msg}
+Script Syntax:
+    
+    python sysargv.py -a  -b -c opt1 opt2 opt3 `list of files / *.extension` "
+    python sysargv.py -h
+
+    -a: Sets a flag
+    -b: Sets b flag
+    -c: Sets c flag
+    opt1: Assigns value to Opt1
+    opt2: Assigns value to Opt2
+    opt3: Assigns value to Opt3
+    list of files: name of files to operated on
+"""
+    )
+
+
+flagh = flaga = flagb = flagc = opt1 = opt2 = opt3 = None
+
+# there should be a minimum of 1 arg when the code is executed else print help
+if len(argv) >= 2 and len(argv) <= 4:
+    print(f"First arg is: {argv[0]}")
+
+    if argv[1] == "-h":
+        flagh = 1
+        print_help(msg="called using -h flag")
+
+    elif "-" in argv[1]:
+        flaga = 1
+        print(f"Flag 1 is: {argv[1]}")
+    else:
+        opt1 = 1
+        print(f"First Option is: {argv[1]}")
+
+elif len(argv) == 5 and "*" in argv[4]:
+    print(f"File wildcard is {argv[4]}")
+
+elif len(argv) > 4:
+    for dx, arg in enumerate(argv[4:]):
+        print("File is {dx} is {arg}")
+else:
+    print_help(msg="Not enough arguments")
+
+# there can be 3 flags, 3 options and n number of files. How to see those args
+# One idea is to check if an argument exists by seeing if it is None or has value
+# Trying to access argv[2] itself will raise index error, so I can enclose
+# each of arg in try except
+
+try:
+    if "-" in argv[2]:
+        print(f"Flag 2 is: {argv[2]}")
+    else:
+        print(f"Option is : {argv[2]}")
+
+except IndexError:
+    print_help(msg="arg 2 is not given")
+
+try:
+    if "-" in argv[3]:
+        print(f"Flag 3 is: {argv[3]}")
+    else:
+        print(f"Option is : {argv[3]}")
+
+except IndexError:
+    print_help(msg="arg 3 is not given")
