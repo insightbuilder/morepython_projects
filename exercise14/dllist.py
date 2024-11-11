048760f035d8b61f12c5af01b335316ccc1475cf
insightbuilder <kamaljp@gmail.com>
2024-11-11 06:43:54 +0530

    started exercise 14

--- exercise14/dllist.py
+++ exercise14/dllist.py
@@ -1,5 +1,17 @@
+from __future__ import annotations
+from typing import Optional
+
+# ruff: noqa : F821
+# pyright:reportUndefinedVariable=false
+
+
 class DNode(object):
-    def __init__(self, val: int, next: DNode | None, prev: DNode | None) -> None:
+    def __init__(
+        self,
+        val: int | str,
+        next: Optional[Dnode] = None,
+        prev: Optional[Dnode] = None,
+    ) -> None:
         self.val = val
         self.next = next
         self.prev = prev
@@ -10,14 +22,53 @@ class DNode(object):
         return f"[{repr(pval)} -> {self.val} <- {repr(nval)}]"
 
 
+# - Are there zero elements? Then self.begin
+#   and self.end need to be None.
+# - If there is one element, then self.begin
+#   and self.end have to be equal (point at
+#   same node).
+# - The ﬁrst element must always have a prev
+#   that is None.
+# - The last element must always have a next
+#   that is None.
+
+
 class Dlist(object):
 
     def __init__(self) -> None:
         self.begin = None
         self.end = None
 
-    def push(self, obj):
+    def _invariant(self):
+        """Checks if the structure is working correctly by
+        ensuring all the invariants mentioned are met"""
+
+    def push(self, obj: str):
         """Appends a new value on the end of the list."""
+        # create Dnode from obj
+        entry = DNode(obj)
+        # if self.begin is none, then assign entry to it
+        if self.begin is None:
+            self.begin = entry
+            self.end = self.begin
+        # else go to the end of the list
+        # keep track of prev also
+        else:
+            # print(f"Obj is {obj}")
+            curr = self.begin
+            prev = curr.prev
+            while True:
+                if curr.next:
+                    curr = curr.next
+                    prev = curr.prev
+                else:
+                    break
+            # need to assign the prev value to curr
+            entry.prev = prev
+            # assign entry as next to that current
+            curr.next = entry
+            # then assign that curr to self.end
+            self.end = entry
 
     def pop(self):
         """Removes the last item and returns it."""
@@ -44,9 +95,39 @@ class Dlist(object):
 
     def count(self):
         """Counts the number of elements in the list."""
+        # if self.begin and end are none, then 0
+        if self.begin is None or self.end is None:
+            return 0
+        # if begin and end are same then 1
+        elif self.begin == self.end:
+            return 1
+        else:
+            # count each element till end of the list
+            curr = self.begin
+            count = 0
+            while True:
+                if curr.next:
+                    curr = curr.next
+                    count += 1
+                else:
+                    return count
 
     def get(self, index):
         """Get the value at index."""
 
     def dump(self):
         """Debugging function that dumps the contents of the list"""
+        outstr = ""
+        if self.begin is None and self.end is None:
+            return "Empty List"
+        elif self.begin:
+            return str(self.begin)
+        else:
+            curr = self.begin
+            while True:
+                if curr.next:
+                    outstr += str(curr)
+                    curr = curr.next
+                else:
+                    break
+        return outstr
